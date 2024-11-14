79672c66707f92302914928843a8e6cdd7f3d684
Euan Simmonds <esimmonds04@gmail.com>
2024-11-14 11:45:48 +0000

    task 1 final

--- task1/task1plot.py
+++ task1/task1plot.py
@@ -5,19 +5,15 @@ from mpl_toolkits.mplot3d import Axes3D
 class rosenblock:
 
     # Define the function
+    @staticmethod
     def f(x, y):
         return 100 * (y - x**2)**2 + (1 - x)**2
 
-    # Create a grid of points
-    def plot():
-        x = np.linspace(-2, 2, 400)
-        y = np.linspace(-2, 2, 400)
-        X, Y = np.meshgrid(x, y)
-        Z = self.f(X, Y)
-
-        # Plot 2D contour
+    def plot(self):
+        
+        self.Z = f(self.X, self.Y)
         plt.figure(self)
-        cp = plt.contourf(X, Y, Z, levels=50, cmap='viridis')
+        cp = plt.contourf(self.X, self.Y, self.Z, levels=50, cmap='viridis')
         plt.colorbar(cp)
         plt.plot(1, 1, 'ro')  # Plot the minimum point
         plt.title('2D Contour Plot')
@@ -28,7 +24,7 @@ class rosenblock:
         # Plot 3D surface
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(X, Y, Z, cmap='viridis', edgecolor='none')
+        ax.plot_surface(self.X, self.Y, self.Z, cmap='viridis', edgecolor='none')
         ax.scatter(1, 1, self.f(1, 1), color='r')  # Plot the minimum point
         ax.set_title('3D Surface Plot')
         ax.set_xlabel('x')
